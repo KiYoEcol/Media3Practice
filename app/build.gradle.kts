d59c4ed71f121247cca5e3acb5ea34a67d36d8a2
KiYo <kiyonori.nakashima@gmail.com>
2024-06-21 19:08:40 +0900

    add media3 exoplayer of dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //Media3 Exoplayer
+    implementation(libs.androidx.media3.exoplayer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
