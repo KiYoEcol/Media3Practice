3b22cc0a3407399438b07c3d4aff718095ede162
KiYo <kiyonori.nakashima@gmail.com>
2024-06-23 19:08:28 +0900

    create InfoContentScreen of Composable

--- app/src/main/java/com/example/media3practice/view/HomeScreen.kt
+++ app/src/main/java/com/example/media3practice/view/HomeScreen.kt
@@ -1,6 +1,33 @@
 package com.example.media3practice.view
 
-import androidx.compose.foundation.layout.fillMaxSize
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
@@ -10,19 +37,37 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
+import com.example.media3practice.R
 
 @Composable
 fun HomeScreen() {
-    VideoPlayerScreen()
+    Column {
+        VideoPlayerScreen()
+        InfoContentScreen(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(horizontal = 12.dp, vertical = 12.dp)
+        )
+    }
 }
 
 @Composable
@@ -84,8 +129,199 @@ fun VideoPlayerScreen() {
             PlayerView(it)
         },
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
+            .aspectRatio(16 / 9f)
     ) {
         it.player = exoPlayer
+    }
+}
+
+@Composable
+fun InfoContentScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "一緒に正解したい！！！！【高校生クイズのアレ】", fontSize = 20.sp)
+        Row {
+            Text(text = "171万回視聴", fontSize = 12.sp, color = Color.Gray)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "4年前", fontSize = 12.sp, color = Color.Gray)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "…その他", fontSize = 12.sp)
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.shell),
+                contentDescription = "User Icon",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(text = "じゃぱんぱん")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "2760", fontSize = 12.sp, color = Color.Gray)
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black)
+            ) {
+                Text(text = "チャンネル登録", color = Color.White)
+            }
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        val horizontalScrollState = rememberScrollState()
+        Row(modifier = Modifier.horizontalScroll(horizontalScrollState)) {
+            AssistChip(
+                onClick = { /*TODO*/ },
+                label = {
+                    Row(modifier = Modifier.clickable { Log.d("TEST", "Click Good") }) {
+                        Row {
+                            Icon(
+                                imageVector = Icons.Outlined.ThumbUp,
+                                contentDescription = "Good",
+                                tint = MaterialTheme.colorScheme.onSurface,
+                                modifier = Modifier.size(AssistChipDefaults.IconSize)
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(text = "2274")
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Divider(
+                                modifier = Modifier
+                                    .height(AssistChipDefaults.IconSize)
+                                    .width(1.dp),
+                                color = Color.Gray
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Icon(
+                            imageVector = ImageVector.vectorResource(id = R.drawable.thumb_down_24px),
+                            contentDescription = "Bad",
+                            tint = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier
+                                .size(AssistChipDefaults.IconSize)
+                                .clickable { Log.d("TEST", "Click Bad") }
+                        )
+                    }
+                },
+                shape = CircleShape
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            AssistChip(
+                onClick = { /*TODO*/ },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.share),
+                        contentDescription = "Share",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                },
+                label = { Text(text = "共有") },
+                shape = CircleShape
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            AssistChip(
+                onClick = { /*TODO*/ },
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.design_services_24px),
+                        contentDescription = "Remix",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                },
+                label = { Text(text = "リミックス") },
+                shape = CircleShape
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            AssistChip(
+                onClick = { /*TODO*/ },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.download),
+                        contentDescription = "Offline",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                },
+                label = { Text(text = "オフライン") },
+                shape = CircleShape
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            AssistChip(
+                onClick = { /*TODO*/ },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icons8_scissors_50),
+                        contentDescription = "Clip",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                },
+                label = { Text(text = "クリップ") },
+                shape = CircleShape
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            AssistChip(
+                onClick = { /*TODO*/ },
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.bookmark_24px),
+                        contentDescription = "Save",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                },
+                label = { Text(text = "保存") },
+                shape = CircleShape
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            AssistChip(
+                onClick = { /*TODO*/ },
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.flag_24px),
+                        contentDescription = "Report",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                },
+                label = { Text(text = "報告") },
+                shape = CircleShape
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Column(
+            modifier = Modifier
+                .background(
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(8.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "コメント")
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "718", fontSize = 12.sp, color = Color.Gray)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.shell),
+                    contentDescription = "User Icon",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "海外で「作者は上質なコカ◯ンをきめている」→「コカ◯ンきめたくらいでボーボボが書けると思うな」って流れになった話好き",
+                    fontSize = 14.sp,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
     }
 }
